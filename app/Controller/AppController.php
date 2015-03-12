0c37cc7ec8751669e8e5e5461933b27031d9b93b
Sanil Shet <shet.sanil@gmail.com>
2015-03-12 22:33:17 +0530

    Forgot Pin

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -48,7 +48,7 @@ class AppController extends Controller {
             'loginRedirect' => array('controller' => 'user', 'action' => 'welcome'),
             'authError' => 'You are not authorized to view this page',
             'allowedActions' => array('logout','register','doRegisterUser','buildTreeJson',
-                'rebuildARO','callAgain','addNote','viewNote','details','index','upload','uploadimage'),
+                'rebuildARO','callAgain','addNote','viewNote','details','index','upload','uploadimage','doResendPin'),
 //            'authenticate' => array(
 //                'all' => array('userModel' => 'User'),
 //                'Form' => array(),
