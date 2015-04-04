92b2c8239b7979d383ff71ef1ca0fa05da3e2f87
Sanil Shet <shet.sanil@gmail.com>
2015-04-04 19:44:42 +0530

    #1373

--- app/webroot/tree/sidebar.js
+++ app/webroot/tree/sidebar.js
@@ -436,7 +436,9 @@ function SP0() {
         SRR("personalview");
         SSR("personalview", "Full name", (p.p || "") + " " + (p.l || p.q || ""), false);
         SSR("personalview", "First Name", p.p, false);
-        SSR("personalview", "Last Name", p.q, false);
+        if ( p.q != '') {
+            SSR("personalview", "Last Name", p.q, false);
+        }
         if (p.q && p.l && (p.q != p.l)) {
             SSR("personalview", "Surname at birth", p.q, false);
         }
