966a919ee607968fb024d7f7196834455a0a88fb
Sanil Shet <shet.sanil@gmail.com>
2015-04-15 22:11:20 +0530

    add brother/sister

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -48,7 +48,7 @@ class AppController extends Controller {
             'loginRedirect' => array('controller' => 'user', 'action' => 'welcome'),
             'authError' => 'You are not authorized to view this page',
             'allowedActions' => array('logout','register','doRegisterUser','buildTreeJson','buildFamilyJson','insertUser',
-                'rebuildARO','callAgain','addNote','viewNote','details','index','upload','uploadimage','doResendPin'
+                'rebuildARO','callAgain','addNote','viewNote','details','index','upload','uploadimage','doResendPin','insertBrotherData'
                 ,'deleteMember', 'saveVote','showResult','gridArticles','detail', 'getBusinessTypes', 'switchLanguage','getPeople'),
 
 //            'authenticate' => array(
