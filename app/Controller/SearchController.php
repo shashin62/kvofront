8aa526ca3af4c478ee0c9246415776cdb9a5ef8d
Sanil Shet <shet.sanil@gmail.com>
2015-05-04 21:25:16 +0530

    Tree Linkage

--- app/Controller/SearchController.php
+++ app/Controller/SearchController.php
@@ -15,7 +15,7 @@ Class SearchController extends AppController {
      *
      * @var type 
      */
-    public $uses = array('User', 'People', 'Group', 'PeopleGroup','Sister','Brother');
+    public $uses = array('User', 'People', 'Group', 'PeopleGroup', 'Sister', 'Brother');
 
     /**
      *
@@ -58,11 +58,11 @@ Class SearchController extends AppController {
         }
 
         $data = $this->People->search($peopleId);
-        
+
         $loggedUserData = $this->People->search($this->Session->read('User.user_id'));
         $sisters = $this->Sister->getSisters($this->Session->read('User.user_id'));
         $brothers = $this->Brother->getBrothers($this->Session->read('User.user_id'));
-       
+
         foreach ($loggedUserData as $d1 => $d3) {
             $this->peopleIds[] = $d3['id'];
             $this->peopleIds[] = $d3['f_id'];
@@ -83,193 +83,141 @@ Class SearchController extends AppController {
         $this->set('peopleData', $peopleData);
         $this->set('groupData', $groupData);
         $this->set('addressData', $addressData);
-        
-        $treeData =$this->Tree->buildTreeJson($groupData['group_id']);
-//        echo '<pre>';
-//        print_r($treeData);
-//        echo '</pre>';
-
+        $treeData = $this->Tree->buildTreeJson($groupData['group_id']);
         $dataTree = $this->_buildLinkage($treeData['tree'], $this->request->data['id'], $userID, false);
-        
-        
-//        $data1 = $this->People->getFamilyDetails($this->Session->read('User.group_id'));
-//
-//        foreach ($data1 as $d1 => $d2) {
-//            $this->peopleIds[] = $d2['People']['id'];
-//            $this->peopleIds[] = $d2['People']['partner_id'];
-//        }
-//        
-//        $this->peopleIds = array_unique($this->peopleIds);
-//
-//        $familyDetails = $this->Tree->buildFamilyJson($peopleId);
-//
         $searchedName[] = $peopleData['first_name'] . ' ' . $peopleData['last_name'];
-//
-//        $treeData = $this->__getDetails($familyDetails['tree'], $peopleId, false, $userID, $this->request->data['id']);
         $tree = array_merge($searchedName, $dataTree);
-
         $this->set('treeLinkageData', $tree);
     }
     
-    
-    
-    private function _buildLinkage($data, $searchedId, $userId, $flag) 
-    {
+    /**
+     * 
+     * @param type $data
+     * @param type $searchedId
+     * @param type $userId
+     * @param type $flag
+     * @return type
+     */
+    private function _buildLinkage($data, $searchedId, $userId, $flag) {
+        echo '<pre>';
+        print_r($data[$searchedId]);
+        echo '</pre>';
 
+        if ($userId == $data[$searchedId]['f']) {
 
-    //    echo '<pre>';
-     //   print_r($data[$searchedId]);
-
-      //  echo '</pre>';
-        
-
-        
-        
-         if ( $userId  == $data[$searchedId]['f']) {
-           
-             if ($data[$searchedId]['g'] == 'f') {
+            if ($data[$searchedId]['g'] == 'f') {
                 $text = '<span style="font-size:12px;">--<b>Daughter of</b>--></span>';
             } else {
                 $text = '<span style="font-size:12px;">--<b>Son of</b>--></span>';
             }
-             $array[] = $text;
+            $array[] = $text;
             $array[] = $data[$data[$searchedId]['f']]['n'];
-        } else if ( count($data[$searchedId]['c']) && in_array($userId, $data[$searchedId]['c'])) {
+        } else if (count($data[$searchedId]['c']) && in_array($userId, $data[$searchedId]['c'])) {
             $childId = array_search($userId, $data[$searchedId]['c']);
             if ($data[$searchedId]['g'] == 'f') {
                 $text = '<span style="font-size:12px;">--<b>Mother of</b>--></span>';
             } else {
                 $text = '<span style="font-size:12px;">--<b>Father of</b>--></span>';
             }
-             $array[] = $text;
+            $array[] = $text;
             $array[] = $data[$data[$searchedId]['c'][$childId]]['n'];
-            
         } else if (count($data[$searchedId]['bid']) & in_array($userId, $data[$searchedId]['bid'])) {
 
             $brotherId = array_search($userId, $data[$searchedId]['bid']);
-            
+
             if ($data[$searchedId]['g'] == 'f') {
                 $text = '<span style="font-size:12px;">--<b>Sister of</b>--></span>';
             } else {
                 $text = '<span style="font-size:12px;">--<b>Brother of</b>--></span>';
             }
             $array[] = $text;
-           
+
             $array[] = $data[$data[$searchedId]['bid'][$brotherId]]['n'];
-            
-            
-            
-            
         } else if (count($data[$searchedId]['sid']) && in_array($userId, $data[$searchedId]['sid'])) {
             exit;
-        } else if(count(array_intersect($data[$searchedId]['sid'], $this->peopleIds)))
-        {
+        } else if (count(array_intersect($data[$searchedId]['sid'], $this->peopleIds))) {
 
             $common = array_values(array_intersect($data[$searchedId]['sid'], $this->peopleIds));
-             $textLabel = 'Sister of';
+            $textLabel = 'Sister of';
             if ($data[$searchedId]['g'] == 'm') {
                 $textLabel = 'Brother of';
             }
             $text = '<span style="font-size:12px;">--<b>' . $textLabel . ' </b>--></span>';
             $array[] = $text;
             $array[] = $data[$common[0]]['n'];
-             if( $common[0] != $this->Session->read('User.user_id')) {
-            $array1 = $this->_buildLinkage($data, $common[0], $this->Session->read('User.user_id')) ;
-            $array = array_merge($array,$array1);
-             }
-        }
-        else if ($data[$searchedId]['es'] != '' && in_array($data[$searchedId]['es'], $this->peopleIds) )
-        {
+            if ($common[0] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $common[0], $this->Session->read('User.user_id'));
+                $array = array_merge($array, $array1);
+            }
+        } else if ($data[$searchedId]['es'] != '' && in_array($data[$searchedId]['es'], $this->peopleIds)) {
             if ($data[$searchedId]['g'] == 'f') {
                 $text = '<span style="font-size:12px;">--<b>Wife of</b>--></span>';
             } else {
                 $text = '<span style="font-size:12px;">--<b>Husband of</b>--></span>';
             }
-             $array[] = $text;
+            $array[] = $text;
             $array[] = $data[$data[$searchedId]['es']]['n'];
-            if( $data[$searchedId]['es'] != $this->Session->read('User.user_id')) {
-                $array1 = $this->_buildLinkage($data, $data[$searchedId]['es'], $this->Session->read('User.user_id')) ;
-             $array = array_merge($array,$array1);
-            }            
-        }
-        
-         else if ( $userId  == $data[$searchedId]['m']) {
+            if ($data[$searchedId]['es'] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $data[$searchedId]['es'], $this->Session->read('User.user_id'));
+                $array = array_merge($array, $array1);
+            }
+        } else if ($userId == $data[$searchedId]['m']) {
             
-        } 
-        
-        else if(count(array_intersect($data[$searchedId]['c'], $this->peopleIds)))
-        {
+        } else if (count(array_intersect($data[$searchedId]['c'], $this->peopleIds))) {
             $common = array_values(array_intersect($data[$searchedId]['c'], $this->peopleIds));
-             $textLabel = 'Mother of';
+            $textLabel = 'Mother of';
             if ($data[$searchedId]['g'] == 'm') {
                 $textLabel = 'Father of';
             }
             $text = '<span style="font-size:12px;">--<b>' . $textLabel . ' </b>--></span>';
             $array[] = $text;
             $array[] = $data[$common[0]]['n'];
-             if( $common[0] != $this->Session->read('User.user_id')) {
-                $array1 = $this->_buildLinkage($data, $common[0], $this->Session->read('User.user_id')) ;
-                $array = array_merge($array,$array1);
-             }
-        }
-        else if(count($data[$searchedId]['sid']) && is_array($data[$searchedId]['sid']) && $flag == false)
-        {
+            if ($common[0] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $common[0], $this->Session->read('User.user_id'));
+                $array = array_merge($array, $array1);
+            }
+        } else if (count($data[$searchedId]['sid']) && is_array($data[$searchedId]['sid']) && $flag == false) {
 
             $textLabel = 'Sister of';
-            if ( $data[$searchedId]['g'] == 'm') {
-                 $textLabel = 'Brother of';
+            if ($data[$searchedId]['g'] == 'm') {
+                $textLabel = 'Brother of';
             }
             $text = '<span style="font-size:12px;">--<b>' . $textLabel . ' </b>--></span>';
             $array[] = $text;
             $array[] = $data[$data[$searchedId]['sid'][0]]['n'];
 
-              if( $data[$searchedId]['sid'][0] != $this->Session->read('User.user_id')) {
-                 $array1 = $this->_buildLinkage($data, $data[$searchedId]['sid'][0], $this->Session->read('User.user_id'), true) ;
-                 $array = array_merge($array,$array1);
-              }
-        }
-       else if(count($data[$searchedId]['bid']) && is_array($data[$searchedId]['bid']) && $flag == false)
-        {
+            if ($data[$searchedId]['sid'][0] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $data[$searchedId]['sid'][0], $this->Session->read('User.user_id'), true);
+                $array = array_merge($array, $array1);
+            }
+        } else if (count($data[$searchedId]['bid']) && is_array($data[$searchedId]['bid']) && $flag == false) {
 
             $textLabel = 'Sister of';
-            if ( $data[$searchedId]['g'] == 'm') {
-                 $textLabel = 'Brother of';
+            if ($data[$searchedId]['g'] == 'm') {
+                $textLabel = 'Brother of';
             }
             $text = '<span style="font-size:12px;">--<b>' . $textLabel . ' </b>--></span>';
             $array[] = $text;
             $array[] = $data[$data[$searchedId]['bid'][0]]['n'];
 
-              if( $data[$searchedId]['bid'][0] != $this->Session->read('User.user_id')) {
-                 $array1 = $this->_buildLinkage($data, $data[$searchedId]['bid'][0], $this->Session->read('User.user_id'), true) ;
-                 $array = array_merge($array,$array1);
-              }
-	   
-        }
-        else if( $data[$searchedId]['f'] != '') 
-        {
+            if ($data[$searchedId]['bid'][0] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $data[$searchedId]['bid'][0], $this->Session->read('User.user_id'), true);
+                $array = array_merge($array, $array1);
+            }
+        } else if ($data[$searchedId]['f'] != '') {
 
             if ($data[$searchedId]['g'] == 'f') {
                 $text = '<span style="font-size:12px;">--<b>Daughter of</b>--></span>';
             } else {
                 $text = '<span style="font-size:12px;">--<b>Son of</b>--></span>';
             }
-             $array[] = $text;
+            $array[] = $text;
             $array[] = $data[$data[$searchedId]['f']]['n'];
-            if( $data[$searchedId]['f'] != $this->Session->read('User.user_id')) {
-                $array1 = $this->_buildLinkage($data, $data[$searchedId]['f'], $this->Session->read('User.user_id')) ;
-                $array = array_merge($array,$array1);
-             }
+            if ($data[$searchedId]['f'] != $this->Session->read('User.user_id')) {
+                $array1 = $this->_buildLinkage($data, $data[$searchedId]['f'], $this->Session->read('User.user_id'));
+                $array = array_merge($array, $array1);
+            }
         }
-            
-        
-        
-        
         return $array;
     }
-    
-    
-    
-    
-
-
 }
