ffb13e400adf8dff3ddf2ffa80f7423a3c3ce47a
Sanil Shet <shet.sanil@gmail.com>
2015-07-14 21:59:51 +0530

    search

--- app/Model/People.php
+++ app/Model/People.php
@@ -2135,9 +2135,9 @@ GROUP BY p.created_by");
         $options['offset'] = 0;
 		
         $options['fields'] = array('People.id', "People.ext", "CONCAT(People.first_name, ' ', People.last_name) as name");        
-        $options['conditions'] = array('CONCAT_WS( " ", People.first_name, IF(People.gender = "male", People.father,People.partner_name) , People.last_name) like' => '%' . $term . '%');          
-		$options['conditions']['AND'] = array('People.father is not null');          
-		$options['conditions']['AND'] = array('People.partner_name is not null');
+        $options['conditions'] = array('CONCAT_WS( " ", People.first_name, People.last_name) like' => '%' . $term . '%');          
+		//$options['conditions']['AND'] = array('People.father is not null');          
+		//$options['conditions']['AND'] = array('People.partner_name is not null');
 		
         try {
             $userData = $this->find('all', $options);
